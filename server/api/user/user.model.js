05b8191acaf6c3691e077d8417ec29c9a3fc4fea
Geekuillaume <geek.uillaume@gmail.com>
2014-09-11 19:20:17 +0200

    Required the name for the user register

--- server/api/user/user.model.js
+++ server/api/user/user.model.js
@@ -6,8 +6,8 @@ var crypto = require('crypto');
 var authTypes = ['github', 'twitter', 'facebook', 'google'];
 
 var UserSchema = new Schema({
-  name: String,
-  email: { type: String, lowercase: true },
+  name: {type: String, required: true},
+  email: { type: String, lowercase: true, required: true },
   role: {
     type: String,
     default: 'user'
